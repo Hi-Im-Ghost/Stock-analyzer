ee55ece98aec73edd77a169447336414a8c9f70d
Adrian Chmielowiec <adikx107@gmail.com>
2023-11-24 01:48:44 +0100

    adding and testing gru rnn model

--- learn.py
+++ learn.py
@@ -1,8 +1,16 @@
 # Prognozowanie szeregów czasowych przy użyciu rekurencyjnych sieci neuronowych (RNN) w TensorFlow
 import math
 import numpy as np
+from keras.src.optimizers.schedules import ExponentialDecay
 from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import GRU, Bidirectional
+from keras.optimizers import SGD
+import matplotlib.pyplot as plt
 
+scaler = MinMaxScaler(feature_range=(0, 1))
 
 # Funkcja do przygotowania danych do nauki
 # data - dane szeregow czasowych
@@ -30,7 +38,7 @@ def set_data_for_learn(data, percent):
     print(select_data_test.shape)
     # Normalizacja danych aby zwiekszyc efektywnosc
     # uzycie minmaxscaler w celu skalowania zbioru danych od 0 do 1.
-    scaler = MinMaxScaler(feature_range=(0, 1))
+
     # Skalowanie danych
     scaled_data_train = scaler.fit_transform(select_data_train)
     scaled_data_test = scaler.fit_transform(select_data_test)
@@ -72,9 +80,74 @@ def set_data_for_learn(data, percent):
     y_data_test = np.array(y_data_test)
 
     # Przeksztalcenie wymiarow
-    x_data_train = np.reshape(x_data_train, (x_data_train.shape[0], x_data_train.shape[1], 1)) # 3 wymiary, 1 - liczba probek, 2 - liczba krokow czasowych (w tym przypadku 50, poniewaz uzywamy 50 poprzednich punktow do prognozowania), 3 - liczba cech
-    y_data_train = np.reshape(y_data_train, (y_data_train.shape[0], 1)) # 2 wymiary, 1 - liczba probek, 2 - liczba krokow
+    x_data_train = np.reshape(x_data_train, (x_data_train.shape[0], x_data_train.shape[1],
+                                             1))  # 3 wymiary, 1 - liczba probek, 2 - liczba krokow czasowych (w tym przypadku 50, poniewaz uzywamy 50 poprzednich punktow do prognozowania), 3 - liczba cech
+    y_data_train = np.reshape(y_data_train,
+                              (y_data_train.shape[0], 1))  # 2 wymiary, 1 - liczba probek, 2 - liczba krokow
     x_data_test = np.reshape(x_data_test, (x_data_test.shape[0], x_data_test.shape[1], 1))
     y_data_test = np.reshape(y_data_test, (y_data_test.shape[0], 1))
     print("x_data_train :", x_data_train.shape, "y_data_train :", y_data_train.shape)
     print("x_data_train :", x_data_test.shape, "y_data_train :", y_data_test.shape)
+
+    # Zwrocenie danych
+    return train_data, test_data,x_data_train, y_data_train, x_data_test, y_data_test
+
+
+# Funkcja do obslugi rekurencyjnej sieci neuronowej
+# Model RNN z warstwa GRU w Keras. Zawiera 4 warstwy GRU i 1 wyjsciowa
+# Funkcja aktywacji tanh. W celu nadmiernego uczenia uzywa warstwy dropout
+# Uzywa opytmalizatora SGD (0.01 uczenia, 1e-7 zaniku, ped 0.9, Nesterov - False
+# Funkcja straty jest blad sredniokwadratowy
+# Jako metryke oceny uzyto dokladnosci
+# Model trenowany na danych treningowych przez 20 epok, uzywajac partii o wielkosci 2
+def rnn_model(x_data_train, y_data_train, x_data_test, y_data_test):
+    # Inicjalizacja RNN
+    rnn = Sequential()
+
+    # Dodanie warstw GRU i warstwy rezygnacji
+    rnn.add(GRU(units=50, return_sequences=True, input_shape=(x_data_train.shape[1], 1), activation='tanh'))
+    rnn.add(Dropout(0.2))
+
+    rnn.add(GRU(units=50, return_sequences=True, activation='tanh'))
+
+    rnn.add(GRU(units=50, return_sequences=True, activation='tanh'))
+
+    rnn.add(GRU(units=50, activation='tanh'))
+
+    # Dodanie warstwy wyjsciowej
+    rnn.add(Dense(units=1, activation='relu'))
+    # Kompilacja rnn
+    learning_rate_schedule = ExponentialDecay(initial_learning_rate=0.01, decay_steps=10000, decay_rate=0.9)
+    optimizer = SGD(learning_rate=learning_rate_schedule, momentum=0.9, nesterov=False)
+    rnn.compile(optimizer=optimizer, loss='mean_squared_error')
+
+    # Dopasowanie danych
+    rnn.fit(x_data_train, y_data_train, epochs=20, batch_size=1)
+    rnn.summary()
+
+    # Predykcja z wykorzystaniem danych testowych
+    y_rnn = rnn.predict(x_data_test)
+
+    # Przywracanie skali z 0-1 do orginalnej
+    y_rnn_org = scaler.inverse_transform(y_rnn)
+
+    # Zwrocenie danych poddanych predykcji w orginalnej skali
+    return y_rnn_org
+
+
+# Funkcja do przedstawienia predykcji za pomoca wykresu
+def draw_predict(train_data, test_data, rnn_model):
+    # Wykres
+    plt.figure(figsize=(10, 6))
+    plt.plot(train_data.index[150:], train_data.Open[150:], label="train_data", color="b")
+    plt.plot(test_data.index, test_data.Open, label="test_data", color="g")
+    plt.plot(test_data.index[50:], rnn_model, label="y_GRU", color="red")
+
+    # Dodanie legendy, tytułu i etykiet osi
+    plt.legend()
+    plt.title("GRU Predictions")
+    plt.xlabel("Days")
+    plt.ylabel("Open Price")
+
+    # Wyświetlenie wykresu
+    plt.show()
--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import tester
 import learn
 
 usecols = ["Date", "Open", "High", "Low", "Close"]
+
 data = tester.check_data('bitcoin.csv', 'new_bitcoin.csv', usecols)
-learn.set_data_for_learn(data, .8)
+
+train_data, test_data, x_data_train, y_data_train, x_data_test, y_data_test = learn.set_data_for_learn(data, .8)
+
+model = learn.rnn_model(x_data_train, y_data_train, x_data_test, y_data_test)
+
+learn.draw_predict(train_data, test_data, model)
